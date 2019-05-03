6b08f2f5c46573ae3134b6e74189f31c40ad5925
SaiTingHu <987947865@qq.com>
2019-05-03 22:31:10 +0800

    新增UI工具类：长按按钮

---
+++ Other/GlobalTools/RunTime/UI/UIPressButton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace HT.Framework
+{
+    /// <summary>
+    /// UGUI按住按钮
+    /// </summary>
+    [AddComponentMenu("HTFramework/UI/UIPressButton")]
+    [RequireComponent(typeof(Button))]
+    [DisallowMultipleComponent]
+    public sealed class UIPressButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    {
+        public UnityEvent OnPressed;
+        public UnityEvent OnRelease;
+
+        private bool _isPressed;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _isPressed = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (_isPressed)
+            {
+                _isPressed = false;
+                OnRelease.Invoke();
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_isPressed)
+            {
+                _isPressed = false;
+                OnRelease.Invoke();
+            }
+        }
+
+        private void Update()
+        {
+            if (_isPressed)
+            {
+                OnPressed.Invoke();
+            }
+        }
+    }
+}
