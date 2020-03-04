2c88ee30ed23b8acca5f53635c37db0bcbc125db
SaiTingHu <987947865@qq.com>
2020-03-04 20:21:00 +0800

    优化：解决热更新库可以被外部代码访问的漏洞。

--- Editor/Hotfix/HotfixManagerInspector.cs
+++ Editor/Hotfix/HotfixManagerInspector.cs
@@ -154,6 +154,8 @@ namespace HT.Framework
             json["includePlatforms"].Add("Editor");
             json["references"] = new JsonData();
             json["references"].Add("HTFramework.RunTime");
+            json["optionalUnityReferences"] = new JsonData();
+            json["optionalUnityReferences"].Add("TestAssemblies");
             string contentNew = GlobalTools.JsonToString(json);
 
             if (contentOld != contentNew)
@@ -172,6 +174,8 @@ namespace HT.Framework
             json["includePlatforms"].Add("Editor");
             json["references"] = new JsonData();
             json["references"].Add("HTFramework.RunTime");
+            json["optionalUnityReferences"] = new JsonData();
+            json["optionalUnityReferences"].Add("TestAssemblies");
 
             File.WriteAllText(filePath, GlobalTools.JsonToString(json));
             AssetDatabase.Refresh();
