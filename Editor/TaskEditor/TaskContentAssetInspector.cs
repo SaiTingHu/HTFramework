98960239eb28d59c6019b1a55b5e2e3230d6dd2a
SaiTingHu <987947865@qq.com>
2020-06-12 15:22:19 +0800

    优化：任务资源对象检视面板，提供清理丢失了脚本的序列化对象的功能。

--- Editor/TaskEditor/TaskContentAssetInspector.cs
+++ Editor/TaskEditor/TaskContentAssetInspector.cs
@@ -23,6 +23,7 @@ namespace HT.Framework
 
         private int _taskContentCount = 0;
         private int _taskPointCount = 0;
+        private bool _isExistMissed = false;
         private Vector2 _scroll = Vector2.zero;
 
         protected override bool IsEnableRuntimeData
@@ -40,7 +41,10 @@ namespace HT.Framework
             _taskContentCount = Target.Content.Count;
             for (int i = 0; i < Target.Content.Count; i++)
             {
-                _taskPointCount += Target.Content[i].Points.Count;
+                if (Target.Content[i] != null)
+                {
+                    _taskPointCount += Target.Content[i].Points.Count;
+                }
             }
         }
 
@@ -96,21 +100,111 @@ namespace HT.Framework
             }
             GUILayout.EndHorizontal();
 
+            if (_isExistMissed)
+            {
+                GUILayout.BeginHorizontal();
+                GUI.color = Color.red;
+                if (GUILayout.Button("Clear Missed Task"))
+                {
+                    for (int i = 0; i < Target.Content.Count; i++)
+                    {
+                        TaskContentBase content = Target.Content[i];
+                        if (content == null)
+                        {
+                            Target.Content.RemoveAt(i);
+                            i -= 1;
+                        }
+                        else
+                        {
+                            for (int j = 0; j < content.Points.Count; j++)
+                            {
+                                TaskPointBase point = content.Points[j];
+                                if (point == null)
+                                {
+                                    for (int d = 0; d < content.Depends.Count; d++)
+                                    {
+                                        if (content.Depends[d].OriginalPoint == j || content.Depends[d].DependPoint == j)
+                                        {
+                                            content.Depends.RemoveAt(d);
+                                            d -= 1;
+                                        }
+                                        else
+                                        {
+                                            if (content.Depends[d].OriginalPoint > j)
+                                            {
+                                                content.Depends[d].OriginalPoint -= 1;
+                                            }
+                                            if (content.Depends[d].DependPoint > j)
+                                            {
+                                                content.Depends[d].DependPoint -= 1;
+                                            }
+                                        }
+                                    }
+                                    content.Points.RemoveAt(j);
+                                    j -= 1;
+                                }
+                            }
+                        }
+                    }
+                    _isExistMissed = false;
+                    HasChanged();
+                }
+                GUI.color = Color.white;
+                GUILayout.EndHorizontal();
+            }
+            else
+            {
+                GUILayout.BeginHorizontal();
+                GUI.enabled = false;
+                GUILayout.Button("Clear Missed Task");
+                GUI.enabled = true;
+                GUILayout.EndHorizontal();
+            }
+
             _scroll = GUILayout.BeginScrollView(_scroll);
             for (int i = 0; i < Target.Content.Count; i++)
             {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label(i + "." + Target.Content[i].Name);
-                GUILayout.FlexibleSpace();
-                GUILayout.EndHorizontal();
-
-                for (int j = 0; j < Target.Content[i].Points.Count; j++)
+                TaskContentBase content = Target.Content[i];
+                if (content != null)
                 {
                     GUILayout.BeginHorizontal();
-                    GUILayout.Space(20);
-                    GUILayout.Label(j + "." + Target.Content[i].Points[j].Name);
+                    GUILayout.Label(i + "." + content.Name);
                     GUILayout.FlexibleSpace();
                     GUILayout.EndHorizontal();
+
+                    for (int j = 0; j < content.Points.Count; j++)
+                    {
+                        TaskPointBase point = content.Points[j];
+                        if (point != null)
+                        {
+                            GUILayout.BeginHorizontal();
+                            GUILayout.Space(20);
+                            GUILayout.Label(j + "." + point.Name);
+                            GUILayout.FlexibleSpace();
+                            GUILayout.EndHorizontal();
+                        }
+                        else
+                        {
+                            GUILayout.BeginHorizontal();
+                            GUI.color = Color.red;
+                            GUILayout.Space(20);
+                            GUILayout.Label(j + ".<Missing script>");
+                            GUILayout.FlexibleSpace();
+                            GUI.color = Color.white;
+                            GUILayout.EndHorizontal();
+                            _isExistMissed = true;
+                        }
+                    }
+                }
+                else
+                {
+                    GUILayout.BeginHorizontal();
+                    GUI.color = Color.red;
+                    GUILayout.Label(i + ".<Missing script>");
+                    GUILayout.FlexibleSpace();
+                    GUI.color = Color.white;
+                    GUILayout.EndHorizontal();
+                    _isExistMissed = true;
                 }
             }
             GUILayout.EndScrollView();
