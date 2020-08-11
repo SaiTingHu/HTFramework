02ca2cf13c1cd8521de14f13c566915ec4537fd3
SaiTingHu <987947865@qq.com>
2020-08-11 13:13:22 +0800

    优化：简化UITwinkleToolkit。

--- RunTime/Utility/Toolkit/UITwinkleToolkit.cs
+++ RunTime/Utility/Toolkit/UITwinkleToolkit.cs
@@ -12,219 +12,121 @@ namespace HT.Framework
     /// </summary>
     public static class UITwinkleToolkit
     {
-        private static Dictionary<Button, TweenerCore<Color, Color, ColorOptions>> Buttons = new Dictionary<Button, TweenerCore<Color, Color, ColorOptions>>();
-        private static Dictionary<Toggle, TweenerCore<Color, Color, ColorOptions>> Toggles = new Dictionary<Toggle, TweenerCore<Color, Color, ColorOptions>>();
-        private static Dictionary<Image, TweenerCore<Color, Color, ColorOptions>> Images = new Dictionary<Image, TweenerCore<Color, Color, ColorOptions>>();
-        private static Dictionary<Text, TweenerCore<Color, Color, ColorOptions>> Texts = new Dictionary<Text, TweenerCore<Color, Color, ColorOptions>>();
-        private static Dictionary<Dropdown, TweenerCore<Color, Color, ColorOptions>> Dropdowns = new Dictionary<Dropdown, TweenerCore<Color, Color, ColorOptions>>();
-
+        private static Dictionary<Graphic, TweenerCore<Color, Color, ColorOptions>> Graphics = new Dictionary<Graphic, TweenerCore<Color, Color, ColorOptions>>();
+        private static Dictionary<Selectable, TweenerCore<Color, Color, ColorOptions>> Selectables = new Dictionary<Selectable, TweenerCore<Color, Color, ColorOptions>>();
+        
         /// <summary>
-        /// 开启按钮闪烁（只在Normal状态）
+        /// 开启图像控件的闪烁
         /// </summary>
-        /// <param name="button">按钮</param>
+        /// <param name="graphic">图像控件</param>
         /// <param name="color">闪烁的目标颜色</param>
         /// <param name="time">闪烁一次的时间</param>
-        public static void OpenTwinkle(this Button button, Color color, float time = 0.5f)
+        public static void OpenTwinkle(this Graphic graphic, Color color, float time = 0.5f)
         {
-            if (!Buttons.ContainsKey(button))
+            if (!Graphics.ContainsKey(graphic))
             {
                 TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
                 () =>
                 {
-                    return button.colors.normalColor;
+                    return graphic.color;
                 },
                 (c) =>
                 {
-                    ColorBlock block = button.colors;
-                    block.normalColor = c;
-                    button.colors = block;
+                    graphic.color = c;
                 }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
 
-                tweener.startValue = button.colors.normalColor;
-                Buttons.Add(button, tweener);
+                tweener.startValue = graphic.color;
+                Graphics.Add(graphic, tweener);
             }
         }
         /// <summary>
-        /// 关闭按钮闪烁
+        /// 关闭图像控件的闪烁
         /// </summary>
-        /// <param name="button">按钮</param>
-        public static void CloseTwinkle(this Button button)
+        /// <param name="graphic">图像控件</param>
+        public static void CloseTwinkle(this Graphic graphic)
         {
-            if (Buttons.ContainsKey(button))
+            if (Graphics.ContainsKey(graphic))
             {
-                ColorBlock block = button.colors;
-                block.normalColor = Buttons[button].startValue;
+                Color normalColor = Graphics[graphic].startValue;
 
-                Buttons[button].Kill();
-                Buttons.Remove(button);
+                Graphics[graphic].Kill();
+                Graphics.Remove(graphic);
 
-                button.colors = block;
+                graphic.color = normalColor;
             }
         }
         /// <summary>
-        /// 开启开关闪烁（只在Normal状态）
+        /// 开启可选控件的闪烁（只在Normal状态）
         /// </summary>
-        /// <param name="toggle">开关</param>
+        /// <param name="selectable">可选控件</param>
         /// <param name="color">闪烁的目标颜色</param>
         /// <param name="time">闪烁一次的时间</param>
-        public static void OpenTwinkle(this Toggle toggle, Color color, float time = 0.5f)
+        public static void OpenTwinkle(this Selectable selectable, Color color, float time = 0.5f)
         {
-            if (!Toggles.ContainsKey(toggle))
+            if (!Selectables.ContainsKey(selectable) && selectable.targetGraphic != null)
             {
-                TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
-                () =>
+                if (selectable.transition == Selectable.Transition.ColorTint)
                 {
-                    return toggle.colors.normalColor;
-                },
-                (c) =>
-                {
-                    ColorBlock block = toggle.colors;
-                    block.normalColor = c;
-                    toggle.colors = block;
-                }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+                    TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
+                    () =>
+                    {
+                        return selectable.colors.normalColor;
+                    },
+                    (c) =>
+                    {
+                        ColorBlock block = selectable.colors;
+                        block.normalColor = c;
+                        selectable.colors = block;
+                    }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
 
-                tweener.startValue = toggle.colors.normalColor;
-                Toggles.Add(toggle, tweener);
+                    tweener.startValue = selectable.colors.normalColor;
+                    Selectables.Add(selectable, tweener);
+                }
+                else
+                {
+                    TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
+                    () =>
+                    {
+                        return selectable.targetGraphic.color;
+                    },
+                    (c) =>
+                    {
+                        selectable.targetGraphic.color = c;
+                    }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+
+                    tweener.startValue = selectable.targetGraphic.color;
+                    Selectables.Add(selectable, tweener);
+                }
             }
         }
         /// <summary>
-        /// 关闭开关闪烁
+        /// 关闭可选控件的闪烁
         /// </summary>
-        /// <param name="toggle">开关</param>
-        public static void CloseTwinkle(this Toggle toggle)
+        /// <param name="selectable">可选控件</param>
+        public static void CloseTwinkle(this Selectable selectable)
         {
-            if (Toggles.ContainsKey(toggle))
+            if (Selectables.ContainsKey(selectable))
             {
-                ColorBlock block = toggle.colors;
-                block.normalColor = Toggles[toggle].startValue;
-
-                Toggles[toggle].Kill();
-                Toggles.Remove(toggle);
-
-                toggle.colors = block;
-            }
-        }
-        /// <summary>
-        /// 开启图片闪烁
-        /// </summary>
-        /// <param name="image">图片</param>
-        /// <param name="color">闪烁的目标颜色</param>
-        /// <param name="time">闪烁一次的时间</param>
-        public static void OpenTwinkle(this Image image, Color color, float time = 0.5f)
-        {
-            if (!Images.ContainsKey(image))
-            {
-                TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
-                () =>
+                if (selectable.transition == Selectable.Transition.ColorTint)
                 {
-                    return image.color;
-                },
-                (c) =>
+                    ColorBlock block = selectable.colors;
+                    block.normalColor = Selectables[selectable].startValue;
+
+                    Selectables[selectable].Kill();
+                    Selectables.Remove(selectable);
+
+                    selectable.colors = block;
+                }
+                else
                 {
-                    image.color = c;
-                }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+                    Color normalColor = Selectables[selectable].startValue;
 
-                tweener.startValue = image.color;
-                Images.Add(image, tweener);
-            }
-        }
-        /// <summary>
-        /// 关闭图片闪烁
-        /// </summary>
-        /// <param name="image">图片</param>
-        public static void CloseTwinkle(this Image image)
-        {
-            if (Images.ContainsKey(image))
-            {
-                Color normalColor = Images[image].startValue;
+                    Selectables[selectable].Kill();
+                    Selectables.Remove(selectable);
 
-                Images[image].Kill();
-                Images.Remove(image);
-
-                image.color = normalColor;
-            }
-        }
-        /// <summary>
-        /// 开启文本框闪烁
-        /// </summary>
-        /// <param name="text">文本框</param>
-        /// <param name="color">闪烁的目标颜色</param>
-        /// <param name="time">闪烁一次的时间</param>
-        public static void OpenTwinkle(this Text text, Color color, float time = 0.5f)
-        {
-            if (!Texts.ContainsKey(text))
-            {
-                TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
-                () =>
-                {
-                    return text.color;
-                },
-                (c) =>
-                {
-                    text.color = c;
-                }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
-
-                tweener.startValue = text.color;
-                Texts.Add(text, tweener);
-            }
-        }
-        /// <summary>
-        /// 关闭文本框闪烁
-        /// </summary>
-        /// <param name="text">文本框</param>
-        public static void CloseTwinkle(this Text text)
-        {
-            if (Texts.ContainsKey(text))
-            {
-                Color normalColor = Texts[text].startValue;
-
-                Texts[text].Kill();
-                Texts.Remove(text);
-
-                text.color = normalColor;
-            }
-        }
-        /// <summary>
-        /// 开启下拉框闪烁（只在Normal状态）
-        /// </summary>
-        /// <param name="dropdown">下拉框</param>
-        /// <param name="color">闪烁的目标颜色</param>
-        /// <param name="time">闪烁一次的时间</param>
-        public static void OpenTwinkle(this Dropdown dropdown, Color color, float time = 0.5f)
-        {
-            if (!Dropdowns.ContainsKey(dropdown))
-            {
-                TweenerCore<Color, Color, ColorOptions> tweener = DOTween.To(
-                () =>
-                {
-                    return dropdown.colors.normalColor;
-                },
-                (c) =>
-                {
-                    ColorBlock block = dropdown.colors;
-                    block.normalColor = c;
-                    dropdown.colors = block;
-                }, color, time).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
-
-                tweener.startValue = dropdown.colors.normalColor;
-                Dropdowns.Add(dropdown, tweener);
-            }
-        }
-        /// <summary>
-        /// 关闭下拉框闪烁
-        /// </summary>
-        /// <param name="dropdown">下拉框</param>
-        public static void CloseTwinkle(this Dropdown dropdown)
-        {
-            if (Dropdowns.ContainsKey(dropdown))
-            {
-                ColorBlock block = dropdown.colors;
-                block.normalColor = Dropdowns[dropdown].startValue;
-
-                Dropdowns[dropdown].Kill();
-                Dropdowns.Remove(dropdown);
-
-                dropdown.colors = block;
+                    if (selectable.targetGraphic != null)
+                        selectable.targetGraphic.color = normalColor;
+                }
             }
         }
         /// <summary>
@@ -232,59 +134,38 @@ namespace HT.Framework
         /// </summary>
         public static void CloseAllTwinkle()
         {
-            foreach (var button in Buttons)
+            foreach (var graphic in Graphics)
             {
-                if (button.Key)
+                if (graphic.Key)
                 {
-                    ColorBlock block = button.Key.colors;
-                    block.normalColor = button.Value.startValue;
-                    button.Value.Kill();
-                    button.Key.colors = block;
+                    Color normalColor = graphic.Value.startValue;
+                    graphic.Value.Kill();
+                    graphic.Key.color = normalColor;
                 }
             }
-            foreach (var toggle in Toggles)
+            foreach (var selectable in Selectables)
             {
-                if (toggle.Key)
+                if (selectable.Key)
                 {
-                    ColorBlock block = toggle.Key.colors;
-                    block.normalColor = toggle.Value.startValue;
-                    toggle.Value.Kill();
-                    toggle.Key.colors = block;
+                    if (selectable.Key.transition == Selectable.Transition.ColorTint)
+                    {
+                        ColorBlock block = selectable.Key.colors;
+                        block.normalColor = selectable.Value.startValue;
+                        selectable.Value.Kill();
+                        selectable.Key.colors = block;
+                    }
+                    else
+                    {
+                        Color normalColor = selectable.Value.startValue;
+                        selectable.Value.Kill();
+                        if (selectable.Key.targetGraphic != null)
+                            selectable.Key.targetGraphic.color = normalColor;
+                    }
                 }
             }
-            foreach (var image in Images)
-            {
-                if (image.Key)
-                {
-                    Color normalColor = image.Value.startValue;
-                    image.Value.Kill();
-                    image.Key.color = normalColor;
-                }
-            }
-            foreach (var text in Texts)
-            {
-                if (text.Key)
-                {
-                    Color normalColor = text.Value.startValue;
-                    text.Value.Kill();
-                    text.Key.color = normalColor;
-                }
-            }
-            foreach (var dropdown in Dropdowns)
-            {
-                if (dropdown.Key)
-                {
-                    ColorBlock block = dropdown.Key.colors;
-                    block.normalColor = dropdown.Value.startValue;
-                    dropdown.Value.Kill();
-                    dropdown.Key.colors = block;
-                }
-            }
-            Buttons.Clear();
-            Toggles.Clear();
-            Images.Clear();
-            Texts.Clear();
-            Dropdowns.Clear();
+            
+            Graphics.Clear();
+            Selectables.Clear();
         }
     }
 }
