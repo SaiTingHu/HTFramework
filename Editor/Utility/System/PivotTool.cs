d5cefe1a92e6d39ce4644ce7002c8d6762cfa178
SaiTingHu <987947865@qq.com>
2022-06-13 17:37:13 +0800

    新增：PivotTool用于物体根级的中心点移动的自定义工具。

---
+++ Editor/Utility/System/PivotTool.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+namespace HT.Framework
+{
+    [EditorTool("Pivot Tool", typeof(Transform))]
+    internal class PivotTool : EditorTool
+    {
+        private Transform _target;
+        private List<Transform> _childs = new List<Transform>();
+        private GUIContent _gc;
+
+        public override GUIContent toolbarIcon
+        {
+            get
+            {
+                if (_gc == null)
+                {
+                    _gc = new GUIContent();
+                    _gc.image = EditorGUIUtility.IconContent("ToolHandlePivot").image;
+                    _gc.tooltip = "Pivot Tool";
+                }
+                return _gc;
+            }
+        }
+
+        private void OnEnable()
+        {
+            EditorTools.activeToolChanged += ActiveToolChanged;
+            Selection.selectionChanged += ActiveToolChanged;
+        }
+        private void OnDisable()
+        {
+            EditorTools.activeToolChanged -= ActiveToolChanged;
+            Selection.selectionChanged -= ActiveToolChanged;
+        }
+        private void ActiveToolChanged()
+        {
+            if (!EditorTools.IsActiveTool(this))
+                return;
+
+            _target = target as Transform;
+            _childs.Clear();
+            for (int i = 0; i < _target.childCount; i++)
+            {
+                _childs.Add(_target.GetChild(i));
+            }
+        }
+
+        public override void OnToolGUI(EditorWindow window)
+        {
+            if (_target == null)
+                return;
+
+            using (new Handles.DrawingScope())
+            {
+                Handles.Label(_target.position, "       Pivot");
+
+                EditorGUI.BeginChangeCheck();
+                Vector3 newValue = Handles.PositionHandle(_target.position, Quaternion.identity);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(_target, "Move Pivot");
+                    Vector3 dir = newValue - _target.position;
+                    _target.position = newValue;
+                    for (int i = 0; i < _childs.Count; i++)
+                    {
+                        Undo.RecordObject(_childs[i], "Move Pivot");
+                        _childs[i].position -= dir;
+                    }
+                }
+            }
+        }
+    }
+}
