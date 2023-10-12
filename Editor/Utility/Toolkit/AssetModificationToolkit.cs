1d40a2c11e81338a27dc5136aca5bce477dddc3c
SaiTingHu <987947865@qq.com>
2023-10-12 17:49:46 +0800

    新增：AssetModificationToolkit.AddUsingNamespace()，在新建脚本时默认向脚本中添加引用命名空间。

--- Editor/Utility/Toolkit/AssetModificationToolkit.cs
+++ Editor/Utility/Toolkit/AssetModificationToolkit.cs
@@ -8,40 +8,75 @@ namespace HT.Framework
     /// <summary>
     /// 项目资源文件修改监控工具箱
     /// </summary>
-    internal sealed class AssetModificationToolkit : UnityEditor.AssetModificationProcessor
+    public sealed class AssetModificationToolkit : UnityEditor.AssetModificationProcessor
     {
+        /// <summary>
+        /// 新建脚本时默认引用的命名空间
+        /// </summary>
+        private static HashSet<string> UsingNamespaces = new HashSet<string>() { "HT.Framework", "DG.Tweening" };
+
+        /// <summary>
+        /// 添加新建脚本时默认引用的命名空间（建议在类的【静态构造方法】中添加）
+        /// </summary>
+        /// <param name="assembly">命名空间</param>
+        public static void AddUsingNamespace(string usingNamespace)
+        {
+            UsingNamespaces.Add(usingNamespace);
+        }
+
         private static void OnWillCreateAsset(string path)
         {
             path = path.Replace(".meta", "");
             if (path.EndsWith(".cs"))
             {
                 bool isFix = false;
+                bool isInAwake = false;
                 List<string> codes = File.ReadAllLines(path).ToList();
                 for (int i = 0; i < codes.Count; i++)
                 {
                     if (!isFix)
                     {
-                        if (codes[i].Contains(" class ") && codes[i].EndsWith("MonoBehaviour"))
+                        if (codes[i].Contains(" class ") && codes[i].EndsWith(": MonoBehaviour"))
                         {
-                            codes[i] = codes[i].Replace("MonoBehaviour", "HTBehaviour");
+                            codes[i] = codes[i].Replace(": MonoBehaviour", ": HTBehaviour");
+                            codes[i] = codes[i] + ", IUpdateFrame";
                             isFix = true;
+                            continue;
                         }
                     }
                     if (isFix)
                     {
-                        if (codes[i].Contains("{"))
+                        if (codes[i].Contains("// Start"))
                         {
-                            codes.Insert(i + 1, "    //启用自动化");
-                            codes.Insert(i + 2, "    protected override bool IsAutomate => true;");
-                            codes.Insert(i + 3, "    ");
-                            break;
+                            codes[i] = "    //初始化操作在 Awake 中完成（必须确保 base.Awake() 的存在）";
+                        }
+                        else if (codes[i].Contains("void Start()"))
+                        {
+                            codes[i] = "    protected override void Awake()";
+                            isInAwake = true;
+                        }
+                        else if (isInAwake && codes[i].Contains("{"))
+                        {
+                            codes.Insert(i + 1, "        base.Awake();");
+                            isInAwake = false;
+                        }
+                        else if (codes[i].Contains("// Update"))
+                        {
+                            codes[i] = "    //等同于 Update 方法，不过当主框架进入暂停状态时，此方法也会停止调用（Main.Current.Pause = true）";
+                        }
+                        else if (codes[i].Contains("void Update()"))
+                        {
+                            codes[i] = "    public void OnUpdateFrame()";
                         }
                     }
                 }
                 if (isFix)
                 {
-                    codes.Insert(0, "using DG.Tweening;");
-                    codes.Insert(0, "using HT.Framework;");
+                    foreach (var item in UsingNamespaces)
+                    {
+                        codes.Insert(0, $"using {item};");
+                    }
+
                     File.WriteAllLines(path, codes.ToArray());
                     AssetDatabase.Refresh();
                 }
