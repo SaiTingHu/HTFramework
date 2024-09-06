f937e181055cb6ce8ac76a16bb39d09583285ec1
SaiTingHu <987947865@qq.com>
2024-09-06 16:12:35 +0800

    优化：扩充默认委托类型。

--- RunTime/Main/MainPartial.cs
+++ RunTime/Main/MainPartial.cs
@@ -1414,10 +1414,12 @@ namespace HT.Framework
     public delegate void HTFAction<in T>(T arg);
     public delegate void HTFAction<in T1, in T2>(T1 arg1, T2 arg2);
     public delegate void HTFAction<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
+    public delegate void HTFAction<in T1, in T2, in T3, in T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
     public delegate TResult HTFFunc<out TResult>();
     public delegate TResult HTFFunc<in T, out TResult>(T arg);
     public delegate TResult HTFFunc<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
     public delegate TResult HTFFunc<in T1, in T2, in T3, out TResult>(T1 arg1, T2 arg2, T3 arg3);
+    public delegate TResult HTFFunc<in T1, in T2, in T3, in T4, out TResult>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
     public delegate IEnumerator CoroutineAction();
     public delegate IEnumerator CoroutineAction<in T>(T arg);
     public delegate IEnumerator CoroutineAction<in T1, in T2>(T1 arg1, T2 arg2);
